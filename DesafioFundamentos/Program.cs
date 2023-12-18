5103d722526de5be9e3651cfd41bf0cd9baff3be
Edenilson Fabiano <edenilsonsouza.BSI@gmail.com>
2023-12-18 20:26:36 -0300

    Verificando os bugs

--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -6,30 +6,43 @@ Console.OutputEncoding = System.Text.Encoding.UTF8;
 decimal precoInicial = 0;
 decimal precoPorHora = 0;
 
-Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" +
-                  "Digite o preço inicial:");
-precoInicial = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Seja bem-vindo ao sistema de estacionamento!\nDigite o preço inicial:");
 
-Console.WriteLine("Agora digite o preço por hora:");
-precoPorHora = Convert.ToDecimal(Console.ReadLine());
+if (decimal.TryParse(Console.ReadLine(), out precoInicial) && precoInicial >= 0)
+{
+    Console.WriteLine("Agora digite o preço por hora:");
 
-// Instancia a classe Estacionamento, já com os valores obtidos anteriormente
-Estacionamento es = new Estacionamento(precoInicial, precoPorHora);
+    if (decimal.TryParse(Console.ReadLine(), out precoPorHora) && precoPorHora >= 0)
+    {
+        // Instancia a classe Estacionamento, já com os valores obtidos anteriormente
+        Estacionamento es = new Estacionamento(precoInicial, precoPorHora);
+    }
+    else
+    {
+        Console.WriteLine("Preço por hora inválido. O programa será encerrado.");
+        return;
+    }
+}
+else
+{
+    Console.WriteLine("Preço inicial inválido. O programa será encerrado.");
+    return;
+}
+Console.WriteLine("Digite a sua opção:");
+Console.WriteLine("1 - Cadastrar veículo");
+Console.WriteLine("2 - Remover veículo");
+Console.WriteLine("3 - Listar veículos");
+Console.WriteLine("4 - Encerrar");
+Console.Clear();
 
-string opcao = string.Empty;
-bool exibirMenu = true;
-
-// Realiza o loop do menu
 while (exibirMenu)
 {
     Console.Clear();
-    Console.WriteLine("Digite a sua opção:");
-    Console.WriteLine("1 - Cadastrar veículo");
-    Console.WriteLine("2 - Remover veículo");
-    Console.WriteLine("3 - Listar veículos");
-    Console.WriteLine("4 - Encerrar");
+    Console.WriteLine("Digite a sua opção:\n1 - Cadastrar veículo\n2 - Remover veículo\n3 - Listar veículos\n4 - Encerrar");
 
-    switch (Console.ReadLine())
+    string opcao = Console.ReadLine();
+
+    switch (opcao)
     {
         case "1":
             es.AdicionarVeiculo();
@@ -55,5 +68,4 @@ while (exibirMenu)
     Console.WriteLine("Pressione uma tecla para continuar");
     Console.ReadLine();
 }
-
-Console.WriteLine("O programa se encerrou");
+Console.WriteLine("O programa foi encerrado com sucesso.");
